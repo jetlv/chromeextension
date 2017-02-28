d27ddf5a7f076a46c890f547f059e26c3af12413
jetlyu <lc515515@163.com>
2017-02-28 01:47:27 -0800

    commit linux script

---
+++ linux_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: linux_install.sh
+# 
+#         USAGE: ./linux_install.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 02/28/2017 01:46
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+npm install
+
