88d781822163b4293d273b83f118d88d036c85e4
jet <jetlyu521@yahoo.com>
2017-03-10 18:23:53 +0800

    fix density error

--- density.js
+++ density.js
@@ -9,6 +9,7 @@ var fs = require('fs');
 const regex = /[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|\w+/g
 
 function density(bodyText, html, keyword) {
+    fs.writeFileSync('body.text', bodyText);
     var text = htmlToText.fromString(html, {
         wordwrap: false,
         ignoreImage: true,
@@ -18,8 +19,8 @@ function density(bodyText, html, keyword) {
     if (keywords.length == 0) {
         return 0;
     }
-    let isPhrase = keyword.split('\r\n').length > 1 ? true : false;
-    if(!isPhrase) {
+    let isPhrase = keyword.split('\s').length > 1 ? true : false;
+    if (!isPhrase) {
         var count = 0;
         keywords.forEach(function (kw, index, element) {
             if (kw.toLowerCase() == keyword.toLowerCase()) {
@@ -32,7 +33,7 @@ function density(bodyText, html, keyword) {
     } else {
         let matched = bodyText.match(new RegExp(keyword.toString(), "ig"));
         fs.writeFileSync('bodytext.txt', bodyText);
-        let count =  matched ? matched.length : 0;
+        let count = matched ? matched.length : 0;
         let realDensity = count / keywords.length;
         let percentDensity = (100 * realDensity).toFixed(2);
         return percentDensity;
