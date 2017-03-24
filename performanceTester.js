e851f060e67113c9353cf48907473debef0f60e2
jet <jetlyu521@yahoo.com>
2017-03-24 17:31:17 +0800

    per

--- performanceTester.js
+++ performanceTester.js
@@ -5,31 +5,22 @@
 let rp = require('request-promise');
 let Promise = require('bluebird');
 
-let ar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let con = 10;
 let i = 0;
-ar = [];
-while (i < 500) {
+let ar = [];
+while(i < con) {
     i++;
     ar.push(i);
 }
 let start = Date.now();
-let count = 0;
 Promise.map(ar, function (id) {
-    return rp({
-        uri: 'http://localhost:3000/seo?link=http://127.0.0.1:5000/',
-        method: 'GET',
-        json: true
-    }).then(function (body) {
+    return rp({uri: 'http://localhost:3000/seo?link=http://127.0.0.1:5000/', method: 'GET', json : true}).then(function (body) {
         // console.log(body);
         console.log(id + ' was done with code ' + body.code);
-        if (body.code == 1) {
-            count++;
-        }
         return id;
     });
-}, {concurrency: 30}).then(function () {
+}, {concurrency: con}).then(function() {
     console.log(Date.now() - start);
-    console.log(count);
 });
 //
 // let test = [1, 2, 3, 4 ,5];
